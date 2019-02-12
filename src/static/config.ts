328a5f5b394e99ba1521fbf9d3f5f39e9094c6b8
Simon Mumenthaler <simon.mumenthaler@shiftcode.ch>
2019-02-12 12:13:12 +0100

    fix(config): cf-outputs.json import for default and rollup import

--- src/static/config.ts
+++ src/static/config.ts
@@ -1,4 +1,10 @@
-import * as configData from '../cf-outputs.json'
+// Depending on whether rollup is used, cf-outputs.json needs to be imported differently.
+// Since jsons don't have a default exports, we normally need to import using the `* as` syntax.
+// However, rollup creates a synthetic default module and we thus need to import it using the `default as` syntax.
+import * as _configData from '../cf-outputs.json'
+// tslint:disable-next-line:no-duplicate-imports
+import { default as _rollupConfigData } from '../cf-outputs.json'
+const configData = _rollupConfigData || _configData
 
 interface CfOutput {
   OutputKey: string
